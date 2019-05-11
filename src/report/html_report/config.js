4a1a148b5ab2544baf2dc53afaf32ac63a97befb
Olha Lyvyndovych <olha.lyvyndovych@gmail.com>
2019-05-11 13:09:31 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,168 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_entire-document_0_document_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190510-144425\\test_entire-document_0_document_0_tablet_h.png",
+        "selector": "document",
+        "fileName": "test_entire-document_0_document_0_tablet_h.png",
+        "label": "entire-document",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -792,
+            "height": -1429
+          },
+          "misMatchPercentage": "2.53",
+          "analysisTime": 235
+        },
+        "diffImage": "..\\bitmaps_test\\20190510-144425\\failed_diff_test_entire-document_0_document_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_h1-heading_0_h1_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190510-144425\\test_h1-heading_0_h1_0_tablet_h.png",
+        "selector": "h1",
+        "fileName": "test_h1-heading_0_h1_0_tablet_h.png",
+        "label": "h1-heading",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 776,
+            "height": -160
+          },
+          "misMatchPercentage": "11.07",
+          "analysisTime": 179
+        },
+        "diffImage": "..\\bitmaps_test\\20190510-144425\\failed_diff_test_h1-heading_0_h1_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_h2-heading_0_h2nth-of-type100n1_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190510-144425\\test_h2-heading_0_h2nth-of-type100n1_0_tablet_h.png",
+        "selector": "h2:nth-of-type(100n + 1)",
+        "fileName": "test_h2-heading_0_h2nth-of-type100n1_0_tablet_h.png",
+        "label": "h2-heading",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 776,
+            "height": -163
+          },
+          "misMatchPercentage": "9.41",
+          "analysisTime": 66
+        },
+        "diffImage": "..\\bitmaps_test\\20190510-144425\\failed_diff_test_h2-heading_0_h2nth-of-type100n1_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_paragraphs_0_pnth-of-type100n1_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190510-144425\\test_paragraphs_0_pnth-of-type100n1_0_tablet_h.png",
+        "selector": "p:nth-of-type(100n + 1)",
+        "fileName": "test_paragraphs_0_pnth-of-type100n1_0_tablet_h.png",
+        "label": "paragraphs",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -776,
+            "height": -90
+          },
+          "misMatchPercentage": "15.29",
+          "analysisTime": 105
+        },
+        "diffImage": "..\\bitmaps_test\\20190510-144425\\failed_diff_test_paragraphs_0_pnth-of-type100n1_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_lists_0_olnth-of-type100n1_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190510-144425\\test_lists_0_olnth-of-type100n1_0_tablet_h.png",
+        "selector": "ol:nth-of-type(100n + 1)",
+        "fileName": "test_lists_0_olnth-of-type100n1_0_tablet_h.png",
+        "label": "lists",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -776,
+            "height": 120
+          },
+          "misMatchPercentage": "22.94",
+          "analysisTime": 88
+        },
+        "diffImage": "..\\bitmaps_test\\20190510-144425\\failed_diff_test_lists_0_olnth-of-type100n1_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_link_0_anth-of-type100n1_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190510-144425\\test_link_0_anth-of-type100n1_0_tablet_h.png",
+        "selector": "a:nth-of-type(100n + 1)",
+        "fileName": "test_link_0_anth-of-type100n1_0_tablet_h.png",
+        "label": "link",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -124,
+            "height": -163
+          },
+          "misMatchPercentage": "4.40",
+          "analysisTime": 37
+        },
+        "diffImage": "..\\bitmaps_test\\20190510-144425\\failed_diff_test_link_0_anth-of-type100n1_0_tablet_h.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "test"
+});
