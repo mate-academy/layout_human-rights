b3df9ec2bfcd3fc2bd17698e252a608abfa253b5
Grossglokner <senior.andrii.levchenko@gmail.com>
2019-05-16 19:02:27 +0300

    Solution

--- config/backstop/backstopConfig.js
+++ config/backstop/backstopConfig.js
@@ -2,7 +2,7 @@
 // https://github.com/garris/BackstopJS#advanced-scenarios
 const basicScenario = {
   label: 'test', // name of the test
-  url: 'http://localhost:8080',
+  url: 'http://localhost:8080/index.html',
   referenceUrl: 'https://mate-academy.github.io/layout_solutions/human-rights/',
   readyEvent: '',
   readySelector: '',
@@ -16,7 +16,7 @@ const basicScenario = {
   selectorExpansion: true,
   expect: 0,
   misMatchThreshold: 1, // 1% of 100%
-  requireSameDimensions: true
+  requireSameDimensions: true,
 };
 
 module.exports = {
@@ -25,40 +25,40 @@ module.exports = {
     {
       name: 'tablet_h',
       width: 1024,
-      height: 768
-    }
+      height: 768,
+    },
   ],
   scenarios: [
     {
       ...basicScenario,
       label: 'entire-document',
-      selectors: ['document']
+      selectors: ['document'],
     },
     {
       ...basicScenario,
       label: 'h1-heading',
-      selectors: ['h1'] // only one h1 on the page
+      selectors: ['h1'], // only one h1 on the page
     },
     {
       ...basicScenario,
       label: 'h2-heading',
-      selectors: ['h2:nth-of-type(100n + 1)'] // check one h2
+      selectors: ['h2:nth-of-type(100n + 1)'], // check one h2
     },
     {
       ...basicScenario,
       label: 'paragraphs',
-      selectors: ['p:nth-of-type(100n + 1)'] // check one paragraph
+      selectors: ['p:nth-of-type(100n + 1)'], // check one paragraph,
     },
     {
       ...basicScenario,
       label: 'lists',
-      selectors: ['ol:nth-of-type(100n + 1)'] // check one list
+      selectors: ['ol:nth-of-type(100n + 1)'], // check one list,
     },
     {
       ...basicScenario,
       label: 'link',
-      selectors: ['a:nth-of-type(100n + 1)'] // check one link
-    }
+      selectors: ['a:nth-of-type(100n + 1)'], // check one link,
+    },
     // define here scenarios for testing
   ],
   paths: {
@@ -66,15 +66,15 @@ module.exports = {
     bitmaps_test: 'backstop_data/bitmaps_test',
     engine_scripts: 'backstop_data/engine_scripts',
     html_report: 'backstop_data/html_report',
-    ci_report: 'backstop_data/ci_report'
+    ci_report: 'backstop_data/ci_report',
   },
   report: ['browser'],
   engine: 'puppeteer',
   engineOptions: {
-    args: ['--no-sandbox']
+    args: ['--no-sandbox'],
   },
   asyncCaptureLimit: 5,
   asyncCompareLimit: 50,
   debug: false,
-  debugWindow: false
+  debugWindow: false,
 };
