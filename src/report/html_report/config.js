69c72020db2ae5b7dca6a74aa55afb6b18ed4e11
Simon <semen.zelenskiy@gmail.com>
2019-08-28 10:29:32 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,149 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/0734900504_entire-document_0_document_0_tablet_h.png",
+        "test": "../bitmaps_test/20190828-102435/0734900504_entire-document_0_document_0_tablet_h.png",
+        "selector": "document",
+        "fileName": "0734900504_entire-document_0_document_0_tablet_h.png",
+        "label": "entire-document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/0734900504_h1-heading_0_h1_0_tablet_h.png",
+        "test": "../bitmaps_test/20190828-102435/0734900504_h1-heading_0_h1_0_tablet_h.png",
+        "selector": "h1",
+        "fileName": "0734900504_h1-heading_0_h1_0_tablet_h.png",
+        "label": "h1-heading",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/0734900504_h2-heading_0_h2nth-of-type100n1_0_tablet_h.png",
+        "test": "../bitmaps_test/20190828-102435/0734900504_h2-heading_0_h2nth-of-type100n1_0_tablet_h.png",
+        "selector": "h2:nth-of-type(100n + 1)",
+        "fileName": "0734900504_h2-heading_0_h2nth-of-type100n1_0_tablet_h.png",
+        "label": "h2-heading",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/0734900504_paragraphs_0_pnth-of-type100n1_0_tablet_h.png",
+        "test": "../bitmaps_test/20190828-102435/0734900504_paragraphs_0_pnth-of-type100n1_0_tablet_h.png",
+        "selector": "p:nth-of-type(100n + 1)",
+        "fileName": "0734900504_paragraphs_0_pnth-of-type100n1_0_tablet_h.png",
+        "label": "paragraphs",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/0734900504_lists_0_olnth-of-type100n1_0_tablet_h.png",
+        "test": "../bitmaps_test/20190828-102435/0734900504_lists_0_olnth-of-type100n1_0_tablet_h.png",
+        "selector": "ol:nth-of-type(100n + 1)",
+        "fileName": "0734900504_lists_0_olnth-of-type100n1_0_tablet_h.png",
+        "label": "lists",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/0734900504_link_0_anth-of-type100n1_0_tablet_h.png",
+        "test": "../bitmaps_test/20190828-102435/0734900504_link_0_anth-of-type100n1_0_tablet_h.png",
+        "selector": "a:nth-of-type(100n + 1)",
+        "fileName": "0734900504_link_0_anth-of-type100n1_0_tablet_h.png",
+        "label": "link",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
